f2f2f65d2d4799dc0d3b477ad436b23a09aa4b52
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2024-12-24 14:45:30 +0100

    Ensure logs persist

--- v8-data-migration/run-data-migration.sh
+++ v8-data-migration/run-data-migration.sh
@@ -1,3 +1,3 @@
 cd /root/ot-node/current/v8-data-migration/ &&
 npm rebuild sqlite3 &&
-nohup node v8-data-migration.js > /root/ot-node/current/v8-data-migration/nohup.out 2>&1 &
+nohup node v8-data-migration.js > /root/ot-node/data/data-migration/logs/nohup.out 2>&1 &

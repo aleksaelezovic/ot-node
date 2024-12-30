0ca8deb0b27cc9058ea81268c3f123a484335d4d
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-12-30 12:22:09 +0100

    Fix update blockchain events migration

--- src/modules/repository/implementation/sequelize/migrations/20241201152000-update-blockchain-events.js
+++ src/modules/repository/implementation/sequelize/migrations/20241201152000-update-blockchain-events.js
@@ -1,37 +1,70 @@
 export async function up({ context: { queryInterface, Sequelize } }) {
-    await queryInterface.renameColumn('blockchain_event', 'blockchain_id', 'blockchain');
+    // Helper function to check if a column exists
+    async function columnExists(table, column) {
+        const tableDescription = await queryInterface.describeTable(table);
+        return Object.prototype.hasOwnProperty.call(tableDescription, column);
+    }
 
-    await queryInterface.changeColumn('blockchain_event', 'block', {
-        type: Sequelize.BIGINT,
-    });
+    if (await columnExists('blockchain_event', 'blockchain_id')) {
+        await queryInterface.renameColumn('blockchain_event', 'blockchain_id', 'blockchain');
+    }
 
-    await queryInterface.renameColumn('blockchain_event', 'block', 'block_number');
+    if (await columnExists('blockchain_event', 'block')) {
+        await queryInterface.changeColumn('blockchain_event', 'block', {
+            type: Sequelize.BIGINT,
+        });
 
-    await queryInterface.addColumn('blockchain_event', 'transaction_index', {
-        type: Sequelize.BIGINT,
-    });
+        await queryInterface.renameColumn('blockchain_event', 'block', 'block_number');
+    }
 
-    await queryInterface.addColumn('blockchain_event', 'log_index', {
-        type: Sequelize.BIGINT,
-    });
+    if (!(await columnExists('blockchain_event', 'transaction_index'))) {
+        await queryInterface.addColumn('blockchain_event', 'transaction_index', {
+            type: Sequelize.BIGINT,
+        });
+    }
 
-    await queryInterface.addColumn('blockchain_event', 'contract_address', {
-        type: Sequelize.STRING,
-    });
+    if (!(await columnExists('blockchain_event', 'log_index'))) {
+        await queryInterface.addColumn('blockchain_event', 'log_index', {
+            type: Sequelize.BIGINT,
+        });
+    }
+
+    if (!(await columnExists('blockchain_event', 'contract_address'))) {
+        await queryInterface.addColumn('blockchain_event', 'contract_address', {
+            type: Sequelize.STRING,
+        });
+    }
 }
 
 export async function down({ context: { queryInterface, Sequelize } }) {
-    await queryInterface.renameColumn('blockchain_event', 'block_number', 'block');
+    async function columnExists(table, column) {
+        const tableDescription = await queryInterface.describeTable(table);
+        return Object.prototype.hasOwnProperty.call(tableDescription, column);
+    }
 
-    await queryInterface.changeColumn('blockchain_event', 'block', {
-        type: Sequelize.INTEGER,
-    });
+    if (await columnExists('blockchain_event', 'block_number')) {
+        await queryInterface.renameColumn('blockchain_event', 'block_number', 'block');
+    }
 
-    await queryInterface.renameColumn('blockchain_event', 'blockchain', 'blockchain_id');
+    if (await columnExists('blockchain_event', 'block')) {
+        await queryInterface.changeColumn('blockchain_event', 'block', {
+            type: Sequelize.INTEGER,
+        });
+    }
 
-    await queryInterface.removeColumn('blockchain_event', 'transaction_index');
+    if (await columnExists('blockchain_event', 'blockchain')) {
+        await queryInterface.renameColumn('blockchain_event', 'blockchain', 'blockchain_id');
+    }
 
-    await queryInterface.removeColumn('blockchain_event', 'log_index');
+    if (await columnExists('blockchain_event', 'transaction_index')) {
+        await queryInterface.removeColumn('blockchain_event', 'transaction_index');
+    }
 
-    await queryInterface.removeColumn('blockchain_event', 'contract_address');
+    if (await columnExists('blockchain_event', 'log_index')) {
+        await queryInterface.removeColumn('blockchain_event', 'log_index');
+    }
+
+    if (await columnExists('blockchain_event', 'contract_address')) {
+        await queryInterface.removeColumn('blockchain_event', 'contract_address');
+    }
 }
